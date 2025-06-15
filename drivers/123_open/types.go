b2a17e9a5a424091dd9e4c34570bd44fecb2890a
xxxxxfpx <112059437+xxxxxfpx@users.noreply.github.com>
2025-06-15 17:33:15 +0800

    Update drivers/123_open/types.go
    Co-authored-by: MadDogOwner <xiaoran@xrgzs.top>
    Signed-off-by: xxxxxfpx <112059437+xxxxxfpx@users.noreply.github.com>

--- drivers/123_open/types.go
+++ drivers/123_open/types.go
@@ -112,6 +112,14 @@ type AccessTokenResp struct {
 	} `json:"data"`
 }
 
+type RefreshTokenResp struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	Scope        string `json:"scope"`
+	TokenType    string `json:"token_type"`
+}
+
 type UserInfoResp struct {
 	BaseResp
 	Data struct {
